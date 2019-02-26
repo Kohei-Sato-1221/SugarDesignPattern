599c84899324a6f973d2b6e3f437bed381210589
kohei.sato <kohei.sato@Obelisk-MacPro.local>
2019-02-26 09:37:18 +0900

    {today}

---
+++ src/sugar/study/designpattern/chapter08/sugarMain.java
@@ -0,0 +1,11 @@
+package sugar.study.designpattern.chapter08;
+
+public class sugarMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hogehoge");
+
+	}
+
+}
